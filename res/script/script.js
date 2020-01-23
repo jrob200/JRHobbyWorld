26d43b401f0e2964c6e2aec17dd11b651133aeb1
JRob <roberts.johns@gmail.com>
2020-01-22 17:51:21 -0800

    HOBBYWEB-12 #comment changed text onclick and revert back onmouseout

--- res/script/script.js
+++ res/script/script.js
@@ -1,4 +1,5 @@
 let prodImg = document.querySelector('.product_image');
+let addToCartButton = document.querySelector('.button');
 
 prodImg.onmouseover = event => {
     event.target.style.opacity = '0.5';
@@ -6,4 +7,12 @@ prodImg.onmouseover = event => {
 
 prodImg.onmouseout = event => {
     event.target.style.opacity = '';
+};
+
+addToCartButton.onclick = event => {
+    event.target.innerHTML = 'Added!'
+};
+
+addToCartButton.onmouseout = event => {
+    event.target.innerHTML = 'Add to Cart';
 };
